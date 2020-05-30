dcbd04b373e41888b68259c9428e8040dd619b7f
Arushi16 <arushi.g151@gmail.com>
2020-05-30 10:56:47 +0530

    Updates

---
+++ precache-manifest.07d2941b832e000d6c612f43f3fd8470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862e1b59d266f13064791903d1ede212",
+    "url": "/Weather-Forecast/index.html"
+  },
+  {
+    "revision": "00c56c52996e81021367",
+    "url": "/Weather-Forecast/static/css/main.543d28f9.chunk.css"
+  },
+  {
+    "revision": "7badd8e980a9e0f2548c",
+    "url": "/Weather-Forecast/static/js/2.677b593e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Forecast/static/js/2.677b593e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c56c52996e81021367",
+    "url": "/Weather-Forecast/static/js/main.bf6fed18.chunk.js"
+  },
+  {
+    "revision": "8cebe56b95154531602d",
+    "url": "/Weather-Forecast/static/js/runtime-main.3cc43f1d.js"
+  }
+]);
